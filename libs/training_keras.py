021cba46a1c20562a415ec16e809a2701ef12868
tanyajoon <57238503+tanyajoon@users.noreply.github.com>
2020-04-30 16:49:17 -0600

    Update training_keras.py

--- libs/training_keras.py
+++ libs/training_keras.py
@@ -1,4 +1,5 @@
 from keras import optimizers, metrics
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from libs import datasets_keras
 from libs.config import LABELMAP
 from libs.util_keras import FBeta
@@ -8,12 +9,12 @@ import wandb
 from wandb.keras import WandbCallback
 
 def train_model(dataset, model):
-    epochs = 15
+    epochs = 50
 #     epochs = 0
-    lr     = 1e-4
+    lr     = 1e-3
     size   = 300
     wd     = 1e-2
-    bs     = 8 # reduce this if you are running out of GPU memory
+    bs     = 4 # reduce this if you are running out of GPU memory
     pretrained = True
 
     config = {
@@ -26,6 +27,25 @@ def train_model(dataset, model):
     }
 
     wandb.config.update(config)
+    checkpointer = ModelCheckpoint('model-resnet50.h5', verbose=1, save_best_only=True)
+    # # Define IoU metric
+    # def mean_iou(y_true, y_pred):
+    #   prec = []
+    #   for t in np.arange(0.5, 1.0, 0.05):
+    #     y_pred_ = tf.to_int32(y_pred > t)
+    #     score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+    #     K.get_session().run(tf.local_variables_initializer())
+    #     with tf.control_dependencies([up_opt]):
+    #         score = tf.identity(score)
+    #     prec.append(score)
+    # return K.mean(K.stack(prec), axis=0)
+
+    earlystopper = EarlyStopping(patience=5, verbose=1)
+    learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
 
     model.compile(
         optimizer=optimizers.Adam(lr=lr),
@@ -36,18 +56,19 @@ def train_model(dataset, model):
             FBeta(name='f_beta')
         ]
     )
-
+    
     train_data, valid_data = datasets_keras.load_dataset(dataset, bs)
     _, ex_data = datasets_keras.load_dataset(dataset, 10)
     model.fit_generator(
         train_data,
         validation_data=valid_data,
         epochs=epochs,
-        callbacks=[
-            WandbCallback(
+        callbacks=[earlystopper,learning_rate_reduction,checkpointer,
+        WandbCallback(
                 input_type='image',
                 output_type='segmentation_mask',
                 validation_data=ex_data[0]
             )
         ]
     )
+  
